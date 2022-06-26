ac04f60a26a34da03839952d5a872d328a13a219
jojo <joakimbwire23@gmail.com>
2022-06-26 13:35:08 +0300

    a list of colors

---
+++ muziki_app/lib/settings/setting_colors.dart
@@ -0,0 +1,90 @@
+
+import 'package:flutter/material.dart';
+
+final List<List<Color>> _backOpt = [
+  [
+    Colors.grey[850]!,
+    Colors.grey[900]!,
+    Colors.black,
+  ],
+  [
+    Colors.grey[900]!,
+    Colors.grey[900]!,
+    Colors.black,
+  ],
+  [
+    Colors.grey[900]!,
+    Colors.black,
+  ],
+  [
+    Colors.grey[900]!,
+    Colors.black,
+    Colors.black,
+  ],
+  [
+    Colors.black,
+    Colors.black,
+  ]
+];
+
+final List<List<Color>> _cardOpt = [
+  [
+    Colors.grey[850]!,
+    Colors.grey[850]!,
+    Colors.grey[900]!,
+  ],
+  [
+    Colors.grey[850]!,
+    Colors.grey[900]!,
+    Colors.grey[900]!,
+  ],
+  [
+    Colors.grey[850]!,
+    Colors.grey[900]!,
+    Colors.black,
+  ],
+  [
+    Colors.grey[900]!,
+    Colors.grey[900]!,
+    Colors.black,
+  ],
+  [
+    Colors.grey[900]!,
+    Colors.black,
+  ],
+  [
+    Colors.grey[900]!,
+    Colors.black,
+    Colors.black,
+  ],
+  [
+    Colors.black,
+    Colors.black,
+  ]
+];
+
+final List<List<Color>> _transOpt = [
+  [
+    Colors.grey[850]!.withOpacity(0.8),
+    Colors.grey[900]!.withOpacity(0.9),
+    Colors.black.withOpacity(1),
+  ],
+  [
+    Colors.grey[900]!.withOpacity(0.8),
+    Colors.grey[900]!.withOpacity(0.9),
+    Colors.black.withOpacity(1),
+  ],
+  [
+    Colors.grey[900]!.withOpacity(0.9),
+    Colors.black.withOpacity(1),
+  ],
+  [
+    Colors.grey[900]!.withOpacity(0.9),
+    Colors.black.withOpacity(0.9),
+    Colors.black.withOpacity(1),
+  ],
+  [
+    Colors.black.withOpacity(0.9),
+    Colors.black.withOpacity(1),
+  ]
+];

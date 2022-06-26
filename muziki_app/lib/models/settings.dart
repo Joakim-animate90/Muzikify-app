2d5d319855896e8154b5ed462423277d3b585421
jojo <joakimbwire23@gmail.com>
2022-06-26 12:22:39 +0300

    changes on default values of setting models

--- muziki_app/lib/models/settings.dart
+++ muziki_app/lib/models/settings.dart
@@ -5,24 +5,26 @@ class Settings extends BaseModel {
 
   @HiveField(3)
   String name;
-  @HiveField(4, defaultValue: '')
+  @HiveField(4, defaultValue: true)
   bool? darkMode;
-  @HiveField(5, defaultValue: '')
+  @HiveField(5, defaultValue: false)
   bool? useSystemTheme;
-  @HiveField(6, defaultValue: '')
+  @HiveField(6, defaultValue: 'Teal')
   String? themeColor;
-  @HiveField(7, defaultValue: '')
+  @HiveField(7, defaultValue: 'Grey')
   String? canvasColor;
-  @HiveField(8, defaultValue: '')
+  @HiveField(8, defaultValue: 'Grey900')
   String? cardColor;
-  @HiveField(9, defaultValue: '')
+  @HiveField(9, defaultValue: 2)
   int? backGrad;
-  @HiveField(10, defaultValue: '')
+  @HiveField(10, defaultValue: 4)
   int? cardGrad;
-  @HiveField(11, defaultValue: '')
+  @HiveField(11, defaultValue: 3)
   int? bottomGrad;
-  @HiveField(12, defaultValue: '')
+  @HiveField(12, defaultValue: 400)
   int? colorHue;
+  @HiveField(13, defaultValue: {})
+  Map? userThemes;
   Settings(
       {required id,
         required this.name,
@@ -35,6 +37,7 @@ class Settings extends BaseModel {
         this.cardGrad,
         this.bottomGrad,
         this.colorHue,
+        this.userThemes,
         syncState})
       : super(id: id, syncState: syncState);
 
